4d1de4c14415b2c38d6c0ffc3213ba498deee171
SWC <SWC@DESKTOP-63RFJP8>
2021-04-02 19:21:59 +0900

    04/02 commit

---
+++ src/main/java/kr/swcore/sderp/sopp/dto/SoppFileDataDTO.java
@@ -0,0 +1,56 @@
+package kr.swcore.sderp.sopp.dto;
+
+public class SoppFileDataDTO {
+	private String fileId;
+	private String fileName;
+	private String fileDesc;
+	private String uploadDate;
+	private byte[] fileContent;
+	private int soppNo;
+	private int userNo;
+	
+	public String getFileId() {
+		return fileId;
+	}
+	public void setFileId(String fileId) {
+		this.fileId = fileId;
+	}
+	public String getFileName() {
+		return fileName;
+	}
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+	public String getFileDesc() {
+		return fileDesc;
+	}
+	public void setFileDesc(String fileDesc) {
+		this.fileDesc = fileDesc;
+	}
+	public String getUploadDate() {
+		return uploadDate;
+	}
+	public void setUploadDate(String uploadDate) {
+		this.uploadDate = uploadDate;
+	}
+	public byte[] getFileContent() {
+		return fileContent;
+	}
+	public void setFileContent(byte[] fileContent) {
+		this.fileContent = fileContent;
+	}
+	public int getSoppNo() {
+		return soppNo;
+	}
+	public void setSoppNo(int soppNo) {
+		this.soppNo = soppNo;
+	}
+	public int getUserNo() {
+		return userNo;
+	}
+	public void setUserNo(int userNo) {
+		this.userNo = userNo;
+	}
+	
+	
+}

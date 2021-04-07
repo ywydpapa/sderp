fabff2309f47cd6c56a438d1f4c880c8c81d2325
kururu <gusrl6394@naver.com>
2021-04-07 18:48:33 +0900

    상품상세 모달창 개발4

--- src/main/java/kr/swcore/sderp/product/service/ProductdataService.java
+++ src/main/java/kr/swcore/sderp/product/service/ProductdataService.java
@@ -1,5 +1,6 @@
 package kr.swcore.sderp.product.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import kr.swcore.sderp.product.dto.ProductDTO;
 import kr.swcore.sderp.product.dto.ProductdataDTO;
 
@@ -9,5 +10,5 @@ import java.util.List;
 public interface ProductdataService {
 
 	List<ProductdataDTO> listProductdata(HttpSession session, ProductdataDTO productdataDTO);
-		
+	List<ProductdataDTO> listAjaxDetail	(HttpSession session, String param) throws JsonProcessingException;
 }

9f8bfdadd60deba6108c98de562f7f787747b52d
kururu <gusrl6394@naver.com>
2021-04-21 19:38:46 +0900

    롬북적용

--- src/main/java/kr/swcore/sderp/sopp/dto/SoppDTO.java
+++ src/main/java/kr/swcore/sderp/sopp/dto/SoppDTO.java
@@ -1,10 +1,16 @@
 package kr.swcore.sderp.sopp.dto;
 
 import kr.swcore.sderp.common.dto.PageDTO;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 import java.math.BigDecimal;
 import java.sql.Timestamp;
 
+@Getter
+@Setter
+@ToString
 public class SoppDTO extends PageDTO {
 
 	private int soppNo;
@@ -34,198 +40,9 @@ public class SoppDTO extends PageDTO {
 	private String targetDateto;
 	private String businessType;
 
-	public int getSoppNo() {
-		return soppNo;
-	}
-	public void setSoppNo(int soppNo) {
-		this.soppNo = soppNo;
-	}
-	public int getCompNo() {
-		return compNo;
-	}
-	public void setCompNo(int compNo) {
-		this.compNo = compNo;
-	}
-	public int getUserNo() {
-		return userNo;
-	}
-	public void setUserNo(int userNo) {
-		this.userNo = userNo;
-	}
-	public int getCustNo() {
-		return custNo;
-	}
-	public void setCustNo(int custNo) {
-		this.custNo = custNo;
-	}
-	public int getCustmemberNo() {
-		return custmemberNo;
-	}
-	public void setCustmemberNo(int custmemberNo) {
-		this.custmemberNo = custmemberNo;
-	}
-	public String getSoppTitle() {
-		return soppTitle;
-	}
-	public void setSoppTitle(String soppTitle) {
-		this.soppTitle = soppTitle;
-	}
-	public int getPtncNo() {
-		return ptncNo;
-	}
-	public void setPtncNo(int ptncNo) {
-		this.ptncNo = ptncNo;
-	}
-	public int getBuyrNo() {
-		return buyrNo;
-	}
-	public void setBuyrNo(int buyrNo) {
-		this.buyrNo = buyrNo;
-	}
-	public String getSoppDesc() {
-		return soppDesc;
-	}
-	public void setSoppDesc(String soppDesc) {
-		this.soppDesc = soppDesc;
-	}
-	public BigDecimal getSoppTargetAmt() {
-		return soppTargetAmt;
-	}
-	public void setSoppTargetAmt(BigDecimal soppTargetAmt) {
-		this.soppTargetAmt = soppTargetAmt;
-	}
-	public String getSoppTargetDate() {
-		return soppTargetDate;
-	}
-	public void setSoppTargetDate(String soppTargetDate) {
-		this.soppTargetDate = soppTargetDate;
-	}
-	public String getSoppStatus() {
-		return soppStatus;
-	}
-	public void setSoppStatus(String soppStatus) {
-		this.soppStatus = soppStatus;
-	}
-	public int getSoppSrate() {
-		return soppSrate;
-	}
-	public void setSoppSrate(int soppSrate) {
-		this.soppSrate = soppSrate;
-	}
-	public String getSoppSource() {
-		return soppSource;
-	}
-	public void setSoppSource(String soppSource) {
-		this.soppSource = soppSource;
-	}
-	public String getUserName() {
-		return userName;
-	}
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-	public String getCustName() {
-		return custName;
-	}
-	public void setCustName(String custName) {
-		this.custName = custName;
-	}
-	public String getPtncName() {
-		return ptncName;
-	}
-	public void setPtncName(String ptncName) {
-		this.ptncName = ptncName;
-	}
-	public String getSoppType() {
-		return soppType;
-	}
-	public void setSoppType(String soppType) {
-		this.soppType = soppType;
-	}
-	public String getSoppTypeN() {
-		return soppTypeN;
-	}
-	public void setSoppTypeN(String soppTypeN) {
-		this.soppTypeN = soppTypeN;
-	}
-	public String getSoppStatusN() {
-		return soppStatusN;
-	}
-	public void setSoppStatusN(String soppStatusN) {
-		this.soppStatusN = soppStatusN;
-	}
-	public Timestamp getRegDatetime() {
-		return regDatetime;
-	}
-	public void setRegDatetime(Timestamp regDatetime) {
-		this.regDatetime = regDatetime;
-	}
-	public String getSopp2Desc() {
-		return sopp2Desc;
-	}
-	public void setSopp2Desc(String sopp2Desc) {
-		this.sopp2Desc = sopp2Desc;
-	}
-	public Timestamp getSopp2regDatetime() {
-		return sopp2regDatetime;
-	}
-	public void setSopp2regDatetime(Timestamp sopp2regDatetime) {
-		this.sopp2regDatetime = sopp2regDatetime;
-	}
-	public String getTargetDatefrom() {
-		return targetDatefrom;
-	}
-	public void setTargetDatefrom(String targetDatefrom) {
-		this.targetDatefrom = targetDatefrom;
-	}
-	public String getTargetDateto() {
-		return targetDateto;
-	}
-	public void setTargetDateto(String targetDateto) {
-		this.targetDateto = targetDateto;
-	}
-	public String getBusinessType() {
-		return businessType;
-	}
-	public void setBusinessType(String businessType) {
-		this.businessType = businessType;
-	}
-
-	@Override
-	public String toString() {
-		return "SoppDTO{" +
-				"soppNo=" + soppNo +
-				", compNo=" + compNo +
-				", userNo=" + userNo +
-				", custNo=" + custNo +
-				", custmemberNo=" + custmemberNo +
-				", soppTitle='" + soppTitle + '\'' +
-				", ptncNo=" + ptncNo +
-				", buyrNo=" + buyrNo +
-				", soppDesc='" + soppDesc + '\'' +
-				", soppTargetAmt=" + soppTargetAmt +
-				", soppTargetDate='" + soppTargetDate + '\'' +
-				", soppStatus='" + soppStatus + '\'' +
-				", soppSrate=" + soppSrate +
-				", soppSource='" + soppSource + '\'' +
-				", userName='" + userName + '\'' +
-				", custName='" + custName + '\'' +
-				", ptncName='" + ptncName + '\'' +
-				", soppType='" + soppType + '\'' +
-				", soppTypeN='" + soppTypeN + '\'' +
-				", soppStatusN='" + soppStatusN + '\'' +
-				", sopp2Desc='" + sopp2Desc + '\'' +
-				", sopp2regDatetime=" + sopp2regDatetime +
-				", targetDatefrom='" + targetDatefrom + '\'' +
-				", targetDateto='" + targetDateto + '\'' +
-				", businessType='" + businessType + '\'' +
-				'}';
-	}
-
 	public SoppDTO() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-
 	
 }
--- src/main/java/kr/swcore/sderp/sopp/dto/SoppdataDTO.java
+++ src/main/java/kr/swcore/sderp/sopp/dto/SoppdataDTO.java
@@ -1,7 +1,14 @@
 package kr.swcore.sderp.sopp.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 import java.math.BigDecimal;
 
+@Getter
+@Setter
+@ToString
 public class SoppdataDTO {
 		
 	private int soppdataNo;
@@ -21,137 +28,10 @@ public class SoppdataDTO {
 	private String linkType;
 	private String linkNo;
 	private String attrib;
-	public int getSoppdataNo() {
-		return soppdataNo;
-	}
-	public void setSoppdataNo(int soppdataNo) {
-		this.soppdataNo = soppdataNo;
-	}
-	public int getSoppNo() {
-		return soppNo;
-	}
-	public void setSoppNo(int soppNo) {
-		this.soppNo = soppNo;
-	}
-	public int getUserNo() {
-		return userNo;
-	}
-	public void setUserNo(int userNo) {
-		this.userNo = userNo;
-	}
-	public String getSoppTitle() {
-		return soppTitle;
-	}
-	public void setSoppTitle(String soppTitle) {
-		this.soppTitle = soppTitle;
-	}
-	public int getCatNo() {
-		return catNo;
-	}
-	public void setCatNo(int catNo) {
-		this.catNo = catNo;
-	}
-	public int getProductNo() {
-		return productNo;
-	}
-	public void setProductNo(int productNo) {
-		this.productNo = productNo;
-	}
-	public String getCatTitle() {
-		return catTitle;
-	}
-	public void setCatTitle(String catTitle) {
-		this.catTitle = catTitle;
-	}
-	public String getDataTitle() {
-		return dataTitle;
-	}
-	public void setDataTitle(String dataTitle) {
-		this.dataTitle = dataTitle;
-	}
-	public String getDataDesc() {
-		return dataDesc;
-	}
-	public void setDataDesc(String dataDesc) {
-		this.dataDesc = dataDesc;
-	}
-	public String getDataType() {
-		return dataType;
-	}
-	public void setDataType(String dataType) {
-		this.dataType = dataType;
-	}
-	public int getDataQuanty() {
-		return dataQuanty;
-	}
-	public void setDataQuanty(int dataQuanty) {
-		this.dataQuanty = dataQuanty;
-	}
-	public BigDecimal getDataAmt() {
-		return dataAmt;
-	}
-	public void setDataAmt(BigDecimal dataAmt) {
-		this.dataAmt = dataAmt;
-	}
-	public BigDecimal getDataNetprice() {
-		return dataNetprice;
-	}
-	public void setDataNetprice(BigDecimal dataNetprice) {
-		this.dataNetprice = dataNetprice;
-	}
-	public String getDataRemark() {
-		return dataRemark;
-	}
-	public void setDataRemark(String dataRemark) {
-		this.dataRemark = dataRemark;
-	}
-	public String getLinkType() {
-		return linkType;
-	}
-	public void setLinkType(String linkType) {
-		this.linkType = linkType;
-	}
-	public String getLinkNo() {
-		return linkNo;
-	}
-	public void setLinkNo(String linkNo) {
-		this.linkNo = linkNo;
-	}
-	public String getAttrib() {
-		return attrib;
-	}
-	public void setAttrib(String attrib) {
-		this.attrib = attrib;
-	}
-
-	@Override
-	public String toString() {
-		return "SoppdataDTO{" +
-				"soppdataNo=" + soppdataNo +
-				", soppNo=" + soppNo +
-				", userNo=" + userNo +
-				", soppTitle='" + soppTitle + '\'' +
-				", catNo=" + catNo +
-				", productNo=" + productNo +
-				", catTitle='" + catTitle + '\'' +
-				", dataTitle='" + dataTitle + '\'' +
-				", dataDesc='" + dataDesc + '\'' +
-				", dataType='" + dataType + '\'' +
-				", dataQuanty=" + dataQuanty +
-				", dataAmt=" + dataAmt +
-				", dataNetprice=" + dataNetprice +
-				", dataRemark='" + dataRemark + '\'' +
-				", linkType='" + linkType + '\'' +
-				", linkNo='" + linkNo + '\'' +
-				", attrib='" + attrib + '\'' +
-				'}';
-	}
 
 	public SoppdataDTO() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
-	
-	
 }

878f0b4ea7a694d7b9c024406011d19ecc01b2c6
DongJin Kim <ywpapa@gmail.com>
2021-12-27 16:01:26 +0900

    그룹웨어 java 부분 추가2

--- src/main/java/kr/swcore/sderp/gw/dto/GwDTO.java
+++ src/main/java/kr/swcore/sderp/gw/dto/GwDTO.java
@@ -3,12 +3,40 @@ package kr.swcore.sderp.gw.dto;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.springframework.web.multipart.MultipartFile;
 
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@ToString
 public class GwDTO {
     private Integer userNo;
+    private Integer docNo;
+    private Integer compNo;
+    private String docTitle;
+    private Integer estItemNo;
+    private Integer docCrUserNo;
+    private Integer docType;
+    private Integer docStatus;
+    private Integer linkMasterdocNo;
+    private String docDesc;
+    private Integer docFormNo;
+    private Integer estNo;
+    private Integer custNo;
+    private Integer soppNo;
+    private Integer productNo;
+    private String productName;
+    private String productSpec;
+    private Integer productQty;
+    private BigDecimal productNetprice;
+    private BigDecimal productVat;
+    private BigDecimal productAmount;
+    private String productRemark;
+    private String regDate;
+    private String modDate;
+    private String attrib;
 
-    public GwDTO() {
+    public GwDTO(Integer userNo) {
         super();
     }
 }
